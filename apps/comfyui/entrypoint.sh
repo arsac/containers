469aa87479fdec479c2fe485095ae98d0bb7c452
Mailo Arsac <mailo.arsac@me.com>
2025-06-10 17:05:17 -0700

    comfyui optimizations

--- apps/comfyui/entrypoint.sh
+++ apps/comfyui/entrypoint.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 set -e
 
-# Creates the directories for the models inside of the volume that is mounted from the host
+# Activate virtual environment
+source /app/.venv/bin/activate
+
+
+
+
+# Creates the directories for the models inside the mounted host volume
+if [ -z "$COMFY_HOME" ]; then
+    echo "COMFY_HOME is not set. Please set it to the ComfyUI home directory."
+    exit 1
+fi
+
 echo "Creating directories for models..."
 MODEL_DIRECTORIES=(
     "checkpoints"
@@ -24,19 +35,19 @@ MODEL_DIRECTORIES=(
     "vae_approx"
 )
 for MODEL_DIRECTORY in ${MODEL_DIRECTORIES[@]}; do
-    mkdir -p /opt/comfyui/models/$MODEL_DIRECTORY
+    mkdir -p $MODEL_DIR/$MODEL_DIRECTORY
 done
 
 echo "Creating symlink for ComfyUI Manager..."
-rm --force /opt/comfyui/custom_nodes/ComfyUI-Manager
+rm --force $COMFY_HOME/custom_nodes/ComfyUI-Manager
 ln -s \
-    /opt/comfyui-manager \
-    /opt/comfyui/custom_nodes/ComfyUI-Manager
+    $COMFYUI_MANAGER_HOME \
+    $COMFY_HOME/custom_nodes/ComfyUI-Manager
 
 echo "Installing requirements for custom nodes..."
-for CUSTOM_NODE_DIRECTORY in /opt/comfyui/custom_nodes/*;
+for CUSTOM_NODE_DIRECTORY in $COMFY_HOME/custom_nodes/*;
 do
-    if [ "$CUSTOM_NODE_DIRECTORY" != "/opt/comfyui/custom_nodes/ComfyUI-Manager" ];
+    if [ "$CUSTOM_NODE_DIRECTORY" != "$COMFY_HOME/custom_nodes/ComfyUI-Manager" ];
     then
         if [ -f "$CUSTOM_NODE_DIRECTORY/requirements.txt" ];
         then
