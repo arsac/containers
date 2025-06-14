662ccbbaba14529fdcf18ac841571bc3a9b3297a
Mailo Arsac <mailo.arsac@me.com>
2025-06-14 10:31:22 -0700

    missing docker entry

--- apps/ai-toolkit/entrypoint.sh
+++ apps/ai-toolkit/entrypoint.sh
@@ -1,1 +1,9 @@
 #! /bin/bash
+
+unset UV_SYSTEM_PYTHON
+
+mkdir -p "${VENV_FOLDER}"
+uv venv --system-site-packages --link-mode=copy --allow-existing "${VENV_FOLDER}"
+source "${VENV_FOLDER}/bin/activate"
+
+"$@"
