f5bdb7173f3f1c38b07e20b78141bd0321244faf
Mailo Arsac <mailo.arsac@me.com>
2025-12-31 07:07:57 -0800

    ci: cache layers

--- build-push-local.sh
+++ build-push-local.sh
@@ -1,9 +1,23 @@
-# !/bin/bash
-# Use pushd to change to the app directory of the desired app  in apps/ that was passed in as an argument
+#!/bin/bash
+set -e
+
+# Use pushd to change to the app directory
 pushd apps/$1
-# Build the Docker image
+
+# Build tag and push flag based on whether REGISTRY is set
+if [ -z "$REGISTRY" ]; then
+    TAG="$1:latest"
+    PUSH=""
+else
+    TAG="$REGISTRY/$1:latest"
+    PUSH="--push"
+fi
+
+# Build the Docker image (and push if registry is set)
 docker buildx build \
     --platform linux/amd64 \
-    --tag $REGISTRY/$1:latest \
-    --push \
+    --tag "$TAG" \
+    $PUSH \
     .
+
+popd
